65a8594568e12ac2e54d952b83a30e02d1af02f8
Gustavo Toshi Komura <gtk10@c3sl.ufpr.br>
2013-10-01 11:05:26 -0300

    Add print size of images
    Signed-off-by: Gustavo Toshi Komura <gtk10@c3sl.ufpr.br>

--- detection.cpp
+++ detection.cpp
@@ -14,11 +14,22 @@ int main( int argc, char** argv )
   if( argc != 3 )
   { readme(); return -1; }
 
-  Mat img_1 = imread( argv[1], CV_LOAD_IMAGE_GRAYSCALE );
-  Mat img_2 = imread( argv[2], CV_LOAD_IMAGE_GRAYSCALE );
+  Mat img_1 = imread( argv[ 1], CV_LOAD_IMAGE_GRAYSCALE );
+  Mat img_2 = imread( argv[ 2], CV_LOAD_IMAGE_GRAYSCALE );
 
   if( !img_1.data || !img_2.data )
   { std::cout<< " --(!) Error reading images " << std::endl; return -1; }
+
+  Size s1 = img_1.size();
+  Size s2 = img_2.size();
+
+  int row1 = s1.height;
+  int colum1 = s1.width;
+  int row2 = s2.height;
+  int colum2 = s2.width;
+
+  printf ( "First image %d\n", row1*colum1);
+  printf ( "Second image %d\n", row2*colum2);
 
   //-- Step 1: Detect the keypoints using SURF Detector
   int minHessian = 400;
@@ -37,10 +48,10 @@ int main( int argc, char** argv )
   drawKeypoints( img_2, keypoints_2, img_keypoints_2, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
 
   //-- Show detected (drawn) keypoints
-  imshow("Keypoints 1", img_keypoints_1 );
-  imshow("Keypoints 2", img_keypoints_2 );
+  imshow( "Keypoints 1", img_keypoints_1 );
+  imshow( "Keypoints 2", img_keypoints_2 );
 
-  waitKey(0);
+  waitKey( 0);
 
   return 0;
   }
